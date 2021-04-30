4a6ed256e6294e5f0cc2249cf1388033fd458b74
admin <admin@DESKTOP-I6D11TC>
2021-05-01 00:18:25 +0400

    xzd

---
+++ JavaApplication35/src/javaapplication35/Githubdemo.java
@@ -0,0 +1,11 @@
+
+package javaapplication35;
+
+public class Githubdemo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
